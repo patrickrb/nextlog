12d11a66ea461f66b50c7d09d985c9e756878cbe
Patrick Burns <burnsoft@gmail.com>
2025-08-01 13:50:55 -0500

    fix build error

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useUser } from '@/contexts/UserContext';
 import Navbar from '@/components/Navbar';
